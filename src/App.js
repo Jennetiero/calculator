414d5836e1102ebc6de1bc5c93438bd80164f021
Jeniero <jennethemrayeva2@gmail.com>
2021-12-14 17:24:15 +0500

    create visual

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const ops = ['/', '*', '-', '+', '.'];
+  const updateCalc = value => {
+    setCalc (calc + value);
+  }
+
+  const createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button key={i}>{i}</button>);
+    }
+    return digits;
+  };
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
